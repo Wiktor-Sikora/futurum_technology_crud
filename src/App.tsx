aebeaed5f9b41a5132a936e8c1c6addd8f8a38a7
Wiktor Sikora <95974314+wiktor-sikora@users.noreply.github.com>
2025-07-21 19:43:22 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className={"flex flex-col h-screen w-screen "}>
+            <h1 className="text-red-400">Campaigns</h1>
+        </div>
+    )
+}
+
+export default App
